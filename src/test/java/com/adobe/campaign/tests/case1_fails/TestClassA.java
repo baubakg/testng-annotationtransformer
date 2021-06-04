f83989791e6a6d87485dba784d15f98429123dda
Baubak Gandomi <gandomi@adobe.com>
2021-06-05 00:19:36 +0200

    code clean

--- src/test/java/com/adobe/campaign/tests/case1_fails/TestClassA.java
+++ src/test/java/com/adobe/campaign/tests/case1_fails/TestClassA.java
@@ -2,7 +2,7 @@ package com.adobe.campaign.tests.case1_fails;
 
 import org.testng.annotations.Test;
 
-@Test(description = "My Desscriptione.", groups = { "A" ,"B","C","D"})
+@Test(description = "My Description.", groups = { "A" ,"B","C","D"})
 public class TestClassA {
 
     public void step1_createEventType() {
